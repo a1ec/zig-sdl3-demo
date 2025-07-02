e4bd1ea8742e199607f9eb8e3726c6e6445925cd
a1ec <a1ec>
2025-07-02 23:05:23 +0300

    Separate box

--- breakout/main.zig
+++ breakout/main.zig
@@ -640,62 +640,7 @@ fn sdlAppQuit(appstate: ?*anyopaque, result: anyerror!c.SDL_AppResult) void {
     }
 }
 
-const Box = struct {
-    x: f32,
-    y: f32,
-    w: f32,
-    h: f32,
-
-    fn intersects(a: Box, b: Box) bool {
-        const min_x = b.x - a.w;
-        const max_x = b.x + b.w;
-        if (a.x > min_x and a.x < max_x) {
-            const min_y = b.y - a.h;
-            const max_y = b.y + b.h;
-            if (a.y > min_y and a.y < max_y) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    fn sweepTest(a: Box, a_vel_x: f32, a_vel_y: f32, b: Box, b_vel_x: f32, b_vel_y: f32) ?Collision {
-        const vel_x_inv = 1 / (a_vel_x - b_vel_x);
-        const vel_y_inv = 1 / (a_vel_y - b_vel_y);
-        const min_x = b.x - a.w;
-        const min_y = b.y - a.h;
-        const max_x = b.x + b.w;
-        const max_y = b.y + b.h;
-        const t_min_x = (min_x - a.x) * vel_x_inv;
-        const t_min_y = (min_y - a.y) * vel_y_inv;
-        const t_max_x = (max_x - a.x) * vel_x_inv;
-        const t_max_y = (max_y - a.y) * vel_y_inv;
-        const entry_x = @min(t_min_x, t_max_x);
-        const entry_y = @min(t_min_y, t_max_y);
-        const exit_x = @max(t_min_x, t_max_x);
-        const exit_y = @max(t_min_y, t_max_y);
-
-        const last_entry = @max(entry_x, entry_y);
-        const first_exit = @min(exit_x, exit_y);
-        if (last_entry < first_exit and last_entry < 1 and first_exit > 0) {
-            var sign_x: f32 = 0;
-            var sign_y: f32 = 0;
-            sign_x -= @floatFromInt(@intFromBool(last_entry == t_min_x));
-            sign_x += @floatFromInt(@intFromBool(last_entry == t_max_x));
-            sign_y -= @floatFromInt(@intFromBool(last_entry == t_min_y));
-            sign_y += @floatFromInt(@intFromBool(last_entry == t_max_y));
-            return .{ .t = last_entry, .sign_x = sign_x, .sign_y = sign_y };
-        }
-        return null;
-    }
-
-    const Collision = struct {
-        t: f32,
-        sign_x: f32,
-        sign_y: f32,
-    };
-};
-
+const Box = @import("box.zig").Box;
 const Paddle = struct {
     box: Box,
     src_rect: *const c.SDL_FRect,
